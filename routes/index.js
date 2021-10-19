3c047d4a03981b4b25e3906e70b9093519bd13d2
saldhyy <saldhyy@gmail.com>
2021-10-19 14:24:44 +0700

    add auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("./auth");
 const visitor = require("./visitor");
 
+router.use(auth);
 router.use(visitor);
 
 module.exports = router;

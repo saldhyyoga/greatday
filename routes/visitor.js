26c343d0950aa7d6cfc8d4e5d2093fa12387ceab
saldhyy <saldhyy@gmail.com>
2021-10-19 17:21:15 +0700

    add middleware

--- routes/visitor.js
+++ routes/visitor.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/visitor");
+const middleware = require("../middleware");
 
 router.post("/visitor/add", controller.addVisitor);
 router.get("/visitor-today", controller.visitorToday);
 router.get("/visitors", controller.listVisitorToday);
-router.get("/visitor/find", controller.findVisitor);
+router.get("/visitor/find", middleware.admin, controller.findVisitor);
 
 module.exports = router;

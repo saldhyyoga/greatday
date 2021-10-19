9b4d058be49f6a1fd9d94e9bfd1b8c275c10de3d
saldhyy <saldhyy@gmail.com>
2021-10-19 16:55:13 +0700

    add column gender

--- models/visitor.js
+++ models/visitor.js
@@ -1,29 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class visitor extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  visitor.init({
-    name: DataTypes.STRING,
-    phonenumber: DataTypes.STRING,
-    address: DataTypes.STRING,
-    office: DataTypes.STRING,
-    email: DataTypes.STRING,
-    tujuan: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'visitor',
-    underscored: true,
-  });
-  return visitor;
+	class visitor extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	visitor.init(
+		{
+			name: DataTypes.STRING,
+			phonenumber: DataTypes.STRING,
+			address: DataTypes.STRING,
+			office: DataTypes.STRING,
+			email: DataTypes.STRING,
+			tujuan: DataTypes.TEXT,
+			gender: DataTypes.ENUM("Laki-laki", "Perempuan"),
+		},
+		{
+			sequelize,
+			modelName: "visitor",
+			underscored: true,
+		}
+	);
+	return visitor;
 };

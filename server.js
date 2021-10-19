dfcbbd50baa53e7a7fde92e9634f8ecbf4b996eb
saldhyy <saldhyy@gmail.com>
2021-10-19 14:58:32 +0700

    test

--- server.js
+++ server.js
@@ -9,5 +9,12 @@ app.use(express.json());
 app.use(cors());
 // routes
 app.use("/api/v1", indexRouter);
+app.get("/test", async (req, res) => {
+	try {
+		res.send("hello");
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
